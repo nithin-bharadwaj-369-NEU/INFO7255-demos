6531b56d0152caae4dd6cda1184729bc8c71fa34
icecube-pixel <dornipadu.n@northeastern.edu>
2023-06-04 18:09:28 -0400

    Updated end point path

--- demo-one/src/main/java/com/bharadwaj/demoone/controller/MedicalPlanController.java
+++ demo-one/src/main/java/com/bharadwaj/demoone/controller/MedicalPlanController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
-@RequestMapping("/api/v1/plan")
+@RequestMapping("/v1/plan")
 public class MedicalPlanController {
 
     @Autowired
