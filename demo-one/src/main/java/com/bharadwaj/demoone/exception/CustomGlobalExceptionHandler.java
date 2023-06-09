1e4d5baba3ec3679b035f054a9a0f3f0a4584585
icecube-pixel <dornipadu.n@northeastern.edu>
2023-06-09 14:05:11 -0400

    Added changes in the code to add exception handlers

--- demo-one/src/main/java/com/bharadwaj/demoone/exception/CustomGlobalExceptionHandler.java
+++ demo-one/src/main/java/com/bharadwaj/demoone/exception/CustomGlobalExceptionHandler.java
@@ -1,31 +1,65 @@
 package com.bharadwaj.demoone.exception;
 
 import com.bharadwaj.demoone.response.CustomErrorResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.boot.json.JsonParseException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.validation.ObjectError;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class CustomGlobalExceptionHandler {
 
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<CustomErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
-//        List<String> errors = ex.getBindingResult()
-//                .getAllErrors().stream()
-//                .map(ObjectError::getDefaultMessage)
-//                .collect(Collectors.toList());
-//
-//        CustomErrorResponse errorResponse = new CustomErrorResponse();
-//        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-//        errorResponse.setMessage("Validation error");
-//        errorResponse.setErrors(errors);
-//
-//        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-//    }
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        String detail= ex.getLocalizedMessage();
+        return new ResponseEntity<Object>(new JSONObject().put("Error : ", detail).toString(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(JsonParseException.class)
+    public final ResponseEntity<Object> handleParseException(JSONException ex, WebRequest request) {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getLocalizedMessage());
+        return new ResponseEntity<Object>(new JSONObject().put("Parsing Exception: ", details).toString(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(JSONException.class)
+    public final ResponseEntity<Object> handleResourceNotFoundException(JSONException ex, WebRequest request) {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getLocalizedMessage());
+        return new ResponseEntity<Object>(new JSONObject().put("JsonException: ", details).toString(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException e) {
+        String detail= e.getLocalizedMessage();
+        return new ResponseEntity<Object>(new JSONObject().put("Validation Error", detail).toString(), HttpStatus.BAD_REQUEST);
+    }
+
 }
